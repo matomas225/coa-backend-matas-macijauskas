b30f696a1739d2c40b6c6a9cc60233703335cd85
matomas225 <matomas225@gmail.com>
2025-03-30 17:15:24 +0300

    express cors setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("Success ");
+});
+
+app.get("/data", (req, res) => {
+  res.json({ data: ["item1, item2, item3"] });
+});
+
+app.listen(3000, () => console.log("Backend running on port: 3000"));
