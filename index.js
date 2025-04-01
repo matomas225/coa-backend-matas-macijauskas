2742fd85d0d48e155b359f3b70588c638b55cc3a
matomas225 <matomas225@gmail.com>
2025-04-01 21:59:56 +0300

    mongoose db setup, get users from db

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/myDatabase")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
 const cors = require("cors");
 
 const corsOptions = {
@@ -15,6 +23,24 @@ app.get("/", (req, res) => {
 
 app.get("/data", (req, res) => {
   res.json({ data: ["item1", "item2", "item3"] });
+});
+
+const userSchema = mongoose.Schema({
+  age: Number,
+  lastName: String,
+  movies: [String],
+  name: String,
+});
+
+const userModel = mongoose.model("users", userSchema);
+
+app.get("/getUsers", async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.json(users);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 app.listen(3000, () => console.log("Backend running on port: 3000"));
